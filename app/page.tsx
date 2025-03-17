9630def90976a820e7d525b0d32f16b050750d21
Abdul <ary5054@gmail.com>
2025-03-17 16:55:24 +0500

    Fix deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from '@/components/hero'
-import Pricing from '@/components/pricing'
+import { PricingCards } from "@/components/pricing"
 import DetailedFeatures from '@/components/detailed-features'
 import WhyUseOrizen from '@/components/why-use-orizen'
 import CTASection from '@/components/cta-section'
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Pricing />
-      <DetailedFeatures />
+      <PricingCards />    
+        <DetailedFeatures />
       <WhyUseOrizen />
       <CTASection />
       <LLCBenefits />
