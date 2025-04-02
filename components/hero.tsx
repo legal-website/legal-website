0865dd261d07421986fa26f682a0303af3564ee9
Abdul <ary5054@gmail.com>
2025-04-02 07:31:45 +0500

    Your update message here

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,13 +2,12 @@
 
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
-import { Phone, Star } from "lucide-react"
+import { Phone } from "lucide-react"
 import { ScrollAnimation } from "./GlobalScrollAnimation"
 
 export default function Hero() {
-  // Custom Trustpilot-style rating display
-  // You can update these values manually based on your actual Trustpilot data
-  const rating = 4.2 // Your average rating (e.g., 4.8)
+  // Your actual Trustpilot data
+  const rating = 4.2 // Your average rating
   const reviewCount = 6 // Your total number of reviews
 
   return (
@@ -40,57 +39,51 @@ export default function Hero() {
                 </p>
               </div>
 
-              {/* Custom Trustpilot-style Rating */}
-              <div className="mb-4 sm:mb-6">
-                <a
-                  href="https://www.trustpilot.com/review/orizeninc.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center"
-                >
-                  {/* Trustpilot Logo */}
-                  <div className="flex items-center mr-2">
-                    <svg width="106" height="24" viewBox="0 0 106 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M97.8,14.1V0.3h7.9v2.1h-5.5v3.5h5.1v2.1h-5.1v6.1H97.8z" fill="#191919" />
-                      <path d="M88.9,14.1V0.3h2.4v13.8H88.9z" fill="#191919" />
-                      <path d="M81.5,14.1V0.3h2.3l4.8,7.8V0.3h2.3v13.8h-2.1l-5-8.1v8.1H81.5z" fill="#191919" />
-                      <path d="M70.5,14.1V2.4h-3.7V0.3h9.9v2.1h-3.7v11.7H70.5z" fill="#191919" />
-                      <path d="M61.5,14.1V0.3h7.9v2.1h-5.5v3.5H69v2.1h-5.1v4h5.5v2.1H61.5z" fill="#191919" />
-                      <path d="M54.2,14.1l-4.1-13.8h2.5l2.9,10.3l3-10.3h2.4l-4.1,13.8H54.2z" fill="#191919" />
-                      <path d="M39.8,14.1V0.3h7.9v2.1h-5.5v3.5h5.1v2.1h-5.1v4h5.5v2.1H39.8z" fill="#191919" />
-                      <path d="M35.4,14.1V2.4h-3.7V0.3h9.9v2.1h-3.7v11.7H35.4z" fill="#191919" />
-                      <path
-                        d="M24.7,14.3c-4.1,0-7.3-3.1-7.3-7.2s3.2-7.2,7.3-7.2c2.5,0,4.1,0.7,5.6,2.1l-1.6,1.8c-1.1-1-2.3-1.5-3.9-1.5 c-2.6,0-4.6,2-4.6,4.8c0,2.8,2,4.8,4.6,4.8c1.7,0,2.8-0.5,4-1.6l1.6,1.6C28.9,13.5,27.2,14.3,24.7,14.3z"
-                        fill="#191919"
-                      />
-                      <path d="M12,0.3h-2.3v13.8H12V0.3z" fill="#191919" />
-                      <path d="M0,0.3v13.8h7.8v-2.1H2.4V0.3H0z" fill="#191919" />
-                      <path
-                        d="M12.1,23.7c-6.5,0-11.9-5.3-11.9-11.9h23.7C23.9,18.4,18.6,23.7,12.1,23.7z"
-                        fill="#00B67A"
-                      />
-                      <path d="M12.1,17.1l3.6-0.9l1.5,4.8L12.1,17.1z" fill="#005128" />
-                      <polygon points="16.3,9.4 12.1,9.4 10.3,13.8 12.1,17.1 16.3,9.4" fill="white" />
-                      <polygon points="16.3,9.4 20.6,9.4 21.9,13.8 16.3,9.4" fill="white" />
-                      <polygon points="21.9,13.8 20.6,18.2 17.2,21 21.9,13.8" fill="white" />
-                      <polygon points="7.8,9.4 12.1,9.4 10.3,13.8 7.8,9.4" fill="white" />
-                      <polygon points="7.8,9.4 3.6,9.4 2.3,13.8 7.8,9.4" fill="white" />
-                      <polygon points="2.3,13.8 3.6,18.2 7,21 2.3,13.8" fill="white" />
-                    </svg>
-                  </div>
+              {/* Trustpilot Rating - Matching your actual site */}
+              <div className="mb-4 sm:mb-6 flex items-center">
+                {/* Trustpilot Logo */}
+                <div className="mr-2">
+                  <svg width="80" height="20" viewBox="0 0 80 20" fill="none" xmlns="/trustpilot.svg">
+                    <path d="M72.8324 0H80.0004V19.9909H72.8324V0Z" fill="#00B67A" />
+                    <path d="M76.4164 14.5909L78.4644 13.6364L80.0004 17.6364L76.4164 14.5909Z" fill="#005128" />
+                    <path d="M0 0H7.168V19.9909H0V0Z" fill="#00B67A" />
+                    <path d="M3.584 14.5909L1.536 13.6364L0 17.6364L3.584 14.5909Z" fill="#005128" />
+                    <path d="M15.6 0H22.768V19.9909H15.6V0Z" fill="#00B67A" />
+                    <path d="M19.184 14.5909L17.136 13.6364L15.6 17.6364L19.184 14.5909Z" fill="#005128" />
+                    <path d="M31.2 0H38.368V19.9909H31.2V0Z" fill="#00B67A" />
+                    <path d="M34.784 14.5909L32.736 13.6364L31.2 17.6364L34.784 14.5909Z" fill="#005128" />
+                    <path d="M46.8 0H53.968V19.9909H46.8V0Z" fill="#00B67A" />
+                    <path d="M50.384 14.5909L48.336 13.6364L46.8 17.6364L50.384 14.5909Z" fill="#005128" />
+                    <path d="M62.4 0H69.568V19.9909H62.4V0Z" fill="#00B67A" />
+                    <path d="M65.984 14.5909L63.936 13.6364L62.4 17.6364L65.984 14.5909Z" fill="#005128" />
+                  </svg>
+                </div>
 
-                  {/* Rating Stars */}
-                  <div className="flex items-center">
-                    <div className="flex mr-1">
-                      {[...Array(5)].map((_, i) => (
-                        <Star key={i} className="w-4 h-4 fill-[#00B67A] text-[#00B67A]" fill="#00B67A" />
-                      ))}
-                    </div>
-                    <span className="text-sm font-medium text-gray-700">
-                      {rating} | {reviewCount} reviews
-                    </span>
+                {/* Rating Stars */}
+                <div className="flex items-center">
+                  <div className="flex mr-2">
+                    {[1, 2, 3, 4, 5].map((star) => (
+                      <svg
+                        key={star}
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill={star <= Math.floor(rating) ? "#00B67A" : "none"}
+                        xmlns="/trustpilot.svg"
+                        className="mr-[1px]"
+                      >
+                        <path
+                          d="M8 0L10.2571 5.08631L16 5.87013L11.9428 9.79367L12.9428 15.4957L8 12.8L3.05714 15.4957L4.05714 9.79367L0 5.87013L5.74286 5.08631L8 0Z"
+                          fill={star <= Math.floor(rating) ? "#00B67A" : "#DADADA"}
+                          stroke={star <= Math.floor(rating) ? "#00B67A" : "#DADADA"}
+                        />
+                      </svg>
+                    ))}
                   </div>
-                </a>
+                  <span className="text-sm font-medium text-gray-700">
+                    {rating} | {reviewCount} reviews
+                  </span>
+                </div>
               </div>
             </div>
 
@@ -112,7 +105,7 @@ export default function Hero() {
                   alt="Certified"
                   width={100}
                   height={100}
-                  className="rounded-full animate-rotate-slow w-12 h-12 sm:w-16 sm:h-16 md:w-24 md:h-24"
+                  className="rounded-full animate-rotate-slow w-12 h-12 sm:w-16 sm:h-16 md:w-28 md:h-28"
                 />
               </div>
 
